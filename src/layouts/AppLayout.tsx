c98ab4fe3b955ac9466e1dbfdde4244dd8aecbf4
bmalkes <bmalkes@gmail.com>
2023-04-12 14:53:48 +0300

    feat:add layout home page

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,3 +1,17 @@
-export default function AppLayout() {
-  return null;
+import Header from "src/components/Header";
+import Meta from "src/components/Meta";
+import { meta } from "src/utils/constants";
+
+interface Props2 {
+  children: React.ReactNode;
+}
+
+export default function AppLayout({ children }: Props2) {
+  return (
+    <div className="text-white">
+      <Meta title={meta.title} description={meta.description} />
+      <Header />
+      <div className="container mt-[57px] flex">{children}</div>
+    </div>
+  );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,28 @@
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 import "react-lazy-load-image-component/src/effects/opacity.css";
 import "src/styles/globals.css";
+import NextNProgress from "nextjs-progressbar";
+import { PRIMARY_COLOR } from "src/utils/constants";
+import { trpc } from "src/utils/trpc";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+interface Props {
+  session: Session | null;
 }
 
-export default MyApp;
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<Props>) {
+  return (
+    <SessionProvider session={session}>
+      <NextNProgress options={{ showSpinner: false }} color={PRIMARY_COLOR} />
+      <Toaster />
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+}
+
+export default trpc.withTRPC(MyApp);
